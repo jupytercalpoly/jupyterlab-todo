3f027c78502160b0ff58d67db8685ee4e1ca8fe5
Afshin T. Darian <git@darian.af>
2021-08-09 20:40:04 +0100

    Try to create a widget factory for a .todo file type

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,21 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import React from 'react';
-import { Widget } from '@lumino/widgets';
-import { ReactWidget } from '@jupyterlab/apputils';
+import {
+  ICommandPalette,
+  MainAreaWidget,
+  ReactWidget
+} from '@jupyterlab/apputils';
 import * as Icons from '@jupyterlab/ui-components';
+import {
+  ABCWidgetFactory,
+  DocumentRegistry,
+  DocumentWidget,
+  IDocumentWidget
+} from '@jupyterlab/docregistry';
+import { Widget } from '@lumino/widgets';
+import React from 'react';
 import { TodoCenter } from './TodoCenter';
-import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 
 
 /**
@@ -20,7 +29,6 @@ import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
   id: 'jupyterlab-todo:plugin',
   autoStart: true,
   requires: [ICommandPalette],
-
   activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     const content: Widget = new MyWidget();
     const widget = new MainAreaWidget({ content });
@@ -28,7 +36,62 @@ import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
     widget.title.closable = true;
     widget.title.icon = Icons.jupyterFaviconIcon;
     app.shell.add(widget, 'right', { rank: 500 });
+
+    const ft: DocumentRegistry.IFileType = {
+      name: 'todo',
+      contentType: 'file',
+      fileFormat: 'text',
+      displayName: 'To Do File',
+      extensions: ['.todo'],
+      mimeTypes: ['text/plain']
+    };
+    app.docRegistry.addFileType(ft);
+
+    const factory = new TodoWidgetFactory({
+      name: 'TODOCENTER',
+      fileTypes: ['todo'],
+      defaultFor: ['todo'],
+      readOnly: true
+    });
+    app.docRegistry.addWidgetFactory(factory);
   }
+}
+
+/**
+ * A todo widget.
+ */
+class TodoWidget extends Widget {
+  constructor(context: DocumentRegistry.Context) {
+    super();
+    this.context = context;
+    void context.ready.then(() => {
+      console.log('context is read');
+    });
+  }
+
+  /**
+   * The todo widget's context.
+   */
+   readonly context: DocumentRegistry.Context;
+}
+
+/**
+ * A widget factory for todo files.
+ */
+class TodoWidgetFactory extends ABCWidgetFactory<
+ IDocumentWidget<TodoWidget>
+> {
+ /**
+  * Create a new widget given a context.
+  */
+ protected createNewWidget(
+   context: DocumentRegistry.IContext<DocumentRegistry.IModel>
+ ): IDocumentWidget<TodoWidget> {
+   console.log('context', context);
+   const content = new TodoWidget(context);
+   const widget = new DocumentWidget({ content, context });
+   return widget;
+ }
 }
 
 class MyWidget extends ReactWidget {
